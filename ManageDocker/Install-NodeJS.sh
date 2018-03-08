c77a8b9e2af016c3c5a5fd1349335c704e29fae3
peterhee <pheese@hotmail.com>
2018-03-08 12:39:08 +0100

    Update NodeJS  Script

--- ManageDocker/Install-NodeJS.sh
+++ ManageDocker/Install-NodeJS.sh
@@ -2,3 +2,7 @@ curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh -o 
 sudo chmod +x install_nvm.sh
 sudo ./install_mvm.sh
 source ~/.profile
+nvm install 8.10.0
+nvm alias default 8.10.0
+nvm use default
+node -v
