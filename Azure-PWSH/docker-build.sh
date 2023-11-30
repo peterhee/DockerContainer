d1806978476ed0c99a7fa8adaedc9633c6e22fde
Peter Heese <pheese@hotmail.com>
2023-11-30 15:30:00 +0100

    Update

--- Azure-PWSH/docker-build.sh
+++ Azure-PWSH/docker-build.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 user="pheese"
-name="azure-pwsh"
+name="azure-cli"
+cpu=""
 
-cpu="arm64"
+case "$cpu" in
+     "amd64" ) cpu="amd64";;
+     *) cpu="arm64";;
+esac
+
+
+echo CPU Type $cpu
 
 # Build Docker Container
 if [ -f dockerfile.$cpu ]; then
-    docker build -t docker.io/$user/$name -f dockerfile.$cpu .
-    docker tag docker.io/$user/$name docker.io/$user/$name:$cpu
+    docker build -t docker.io/$user/$name:$cpu -f dockerfile.$cpu .
+    # docker tag docker.io/$user/$name docker.io/$user/$name:$cpu
 fi
