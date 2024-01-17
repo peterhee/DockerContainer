2dfb519c8a6988a505f594f9f0a20b7da3e04832
Peter Heese <pheese@hotmail.com>
2024-01-17 10:22:49 +0100

    Update

---
+++ Azure-PWSH/docker-build.ps1
@@ -0,0 +1,21 @@
+# Command Line Arguments
+for ($i = 0; $i -le $Args.count; $i++ ) {
+	If ($Args[$i].Length -gt 0)
+		{
+		Switch ($Args[$i].ToLower())
+			{
+			"amd64" {
+					$cpu = [String]$Args[$i]}
+			default {
+                $cpu = "arm64"
+                } 
+			}
+		}
+	}
+
+write.host $cpu
+
+if (Test-Path dockerfile.$cpu) {
+    write-host docker build -t docker.io/$user/$name:$cpu -f dockerfile.$cpu .
+}
+
