97e93986feeb93c5441d1b0ac1208600c2d7a26f
Peter Heese <pheese@hotmail.com>
2024-06-25 14:55:45 +0200

    Update dockerfile install.sh

--- Azure-Cli/install.sh
+++ Azure-Cli/install.sh
@@ -4,7 +4,13 @@
 # Bash script to install the Azure CLI
 #
 
-install_script="/home/$1/scripts/install.py"
+$USER=$1
+
+if [ -z $USER ]; then
+    install_script="/root/scripts/install.py"
+else
+    install_script="/home/$1/scripts/install.py"
+fi
 # chmod 775 $install_script
 echo "Running install script."
 python3 $install_script 
